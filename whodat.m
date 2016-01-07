b0674b42f9a831e8e697a9186a51865f4864e1ce
Bernard Roesler <bernard.roesler@gmail.com>
2016-01-07 18:53:53 -0500

    [whodat.m] Update for N-dim arrays

--- whodat.m
+++ whodat.m
@@ -30,7 +30,13 @@ function S = whodat(varargin)
 
 % Structure of variables in calling function workspace (i.e. main)
 A = evalin('caller','whos');
-N = numel(A);
+
+% Make sure we have variables to list
+if isempty(A)
+    return
+else
+    N = numel(A);
+end
 
 % Arrays to be printed in each column
 names       = cell(N,1);
@@ -40,14 +46,26 @@ classes     = cell(N,1);
 mins        = cell(N,1);
 maxes       = cell(N,1);
 attributes  = cell(N,1);
+ndim        = zeros(N,1);
 
 %-------------------------------------------------------------------------------
 %       loop over each variable to build columns
 %-------------------------------------------------------------------------------
 for i = 1:N
-    names{i}   = A(i).name;
-    sizes1{i}  = num2str(A(i).size(1));
-    sizes2{i}  = num2str(A(i).size(2));
+    names{i} = A(i).name;
+
+    % Check for number of dimensions
+    ndim(i) = length(A(i).size);
+
+    sizes1{i} = num2str(A(i).size(1));
+    if (ndim(i) == 2)
+        sizes2{i} = num2str(A(i).size(2));
+    elseif (ndim(i) == 3)
+        sizes2{i} = sprintf('%dx%d', A(i).size(2), A(i).size(3));
+    else
+        sizes2{i} = sprintf('%d-D', ndim(i));
+    end
+
     classes{i} = A(i).class;
 
     % Report actual values if numeric
@@ -95,17 +113,18 @@ end
 %-------------------------------------------------------------------------------
 %       Create format specifiers
 %-------------------------------------------------------------------------------
+% TODO Add formatting for N-dim array sizes (i.e. 5x10x1000)
 % find longest string in each cell, or length of header
 nlen  = max( length('Name'), max(cellfun(@length, names)) );
 slen1 = max( 1, max(cellfun(@length, sizes1)) );
-slen2 = max( 1, max(cellfun(@length, sizes2)) );
+slen2 = max( 2, max(cellfun(@length, sizes2)) );
 clen  = min( 8, max(cellfun(@length, classes)) ); % ignore long matlab classes
 milen = max( length('Min'), max(cellfun(@length, mins)) );
 malen = max( length('Max'), max(cellfun(@length, maxes)) );
 alen  = max( length('Attributes'), max(cellfun(@length, attributes)) );
 
 nfmt  = ['%-' num2str(2+nlen)  '.' num2str(nlen)  's'];
-sfmt  = ['%'  num2str(1+slen1+slen2) 's'];
+sfmt  = ['%-' num2str(1+slen1+slen2) 's'];
 sfmt1 = ['%'  num2str(slen1) 's']; 
 sfmt2 = ['%-' num2str(slen2) 's']; 
 cfmt  = ['%-' num2str(1+clen)  '.' num2str(clen)  's'];
@@ -120,13 +139,19 @@ if nargout == 1
     S = A;  % output with no printing
 else
     % Header line
-    fprintf(['  ' nfmt ' ' sfmt '   ' cfmt ' ' mifmt ' ' mafmt '  ' afmt '\n\n'],...
+    fprintf(['  ' nfmt '  ' sfmt '   ' cfmt ' ' mifmt ' ' mafmt '  ' afmt '\n\n'],...
         'Name', 'Size', 'Class', 'Min', 'Max', 'Attributes')
 
     % List variables
     for i = 1:N
-        fprintf(['  ' nfmt ' ' sfmt1 'x' sfmt2 '   ' cfmt ' ' mifmt ' ' mafmt '  ' afmt '\n'],...
-            names{i}, sizes1{i}, sizes2{i}, classes{i}, mins{i}, maxes{i}, attributes{i}) 
+        if (ndim(i) < 4)
+            fprintf(['  ' nfmt '  ' sfmt1 'x' sfmt2 '   ' cfmt ' ' mifmt ' ' mafmt '  ' afmt '\n'],...
+                names{i}, sizes1{i}, sizes2{i}, classes{i}, mins{i}, maxes{i}, attributes{i}) 
+        else
+            % Need format to ensure class is aligned with the rest of classes
+            fprintf(['  ' nfmt '     ' sfmt cfmt ' ' mifmt ' ' mafmt '  ' afmt '\n'],...
+                names{i}, sizes2{i}, classes{i}, mins{i}, maxes{i}, attributes{i}) 
+        end
     end
     fprintf('\n')
 end
