d375bf99c7c60724b9d05d6d17b074b8286653fa
Bernard Roesler <bernard.roesler@gmail.com>
2016-05-20 13:09:29 -0400

    Remove mail prefs from startup.m. Add keynote colors.

--- mycolors.m
+++ mycolors.m
@@ -58,3 +58,37 @@ color_grimace     =  [ 0.4940    0.1840    0.5560 ];
 color_kermit      =  [ 0.4660    0.6740    0.1880 ];
 color_brightblue  =  [ 0.3010    0.7450    0.9330 ];
 color_pumbaa      =  [ 0.6350    0.0780    0.1840 ];
+
+% Keynote 6 colors
+color_keynote_blue1 = [ 20 151 252]/255;
+color_keynote_blue2 = [  3 101 192]/255;
+color_keynote_blue3 = [ 22  79 134]/255;
+
+color_keynote_red1 = [236  93  87]/255;
+color_keynote_red2 = [200  37   6]/255;
+color_keynote_red3 = [134  16   1]/255;
+
+color_keynote_green1 = [112 191  65]/255;
+color_keynote_green2 = [  0 136  43]/255;
+color_keynote_green3 = [ 11  93  24]/255;
+
+color_keynote_orange1 = [243 144 25]/255;
+color_keynote_orange2 = [222 106 16]/255;
+color_keynote_orange3 = [189 91 12]/255;
+
+color_solarized_base03  = [  0  43  54]/255;
+color_solarized_base02  = [  7  54  66]/255;
+color_solarized_base01  = [ 88 110 117]/255;
+color_solarized_base00  = [101 123 131]/255;
+color_solarized_base0   = [131 148 150]/255;
+color_solarized_base1   = [147 161 161]/255;
+color_solarized_base2   = [238 232 213]/255;
+color_solarized_base3   = [253 246 227]/255;
+color_solarized_yellow  = [181 137   0]/255;
+color_solarized_orange  = [203  75  22]/255;
+color_solarized_red     = [220  50  47]/255;
+color_solarized_magenta = [211  54 130]/255;
+color_solarized_violet  = [108 113 196]/255;
+color_solarized_blue    = [ 38 139 210]/255;
+color_solarized_cyan    = [ 42 161 152]/255;
+color_solarized_green   = [133 153   0]/255;
